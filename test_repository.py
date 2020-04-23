f7c139157b42fe6f8d32c4a513de545f9659312b
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-22 20:52:04 -0400

    todo: Test repository

---
+++ test_repository.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def test_repository_can_save_a_batch(session):
+    pytest.fail('todo')
+
+
+def insert_order_line(session):
+    pytest.fail('todo')
+
+
+def insert_batch(session, batch_id):
+    pytest.fail('todo')
+
+
+def insert_allocation(session, orderline_id, batch_id):
+    pytest.fail('todo')
+
+
+def test_repository_can_retrieve_a_batch_with_allocations(session):
+    pytest.fail('todo')
