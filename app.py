59d6751cb4c82280ae98a7ca8c64659559fbb312
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-22 20:14:15 -0400

    Add stubs for allocate endpoint.

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, make_response
+from model import OrderLine
+app = Flask(__name__)
+
+
+@app.route('/allocate')
+def allocate_endpoint():
+    # TODO: Extract OrderLine data from query params
+    # TODO: Load All Batches from DB
+    # TODO: Call Domain Service
+    # TODO: Save Allocation in DB
+    response = make_response(200)
+    return response
