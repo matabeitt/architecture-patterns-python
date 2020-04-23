d7c083227287cfe8a71131f9f393af3d3ffca1af
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-22 20:14:43 -0400

    Change response to something more appropriate.

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def allocate_endpoint():
     # TODO: Load All Batches from DB
     # TODO: Call Domain Service
     # TODO: Save Allocation in DB
-    response = make_response(200)
+    response = make_response(201)
     return response
