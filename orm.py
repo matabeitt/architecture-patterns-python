c15818aac6b037cf3ccd438cee416235fd04aaac
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-22 20:24:07 -0400

    orderline orm that depends on orderline model

---
+++ orm.py
@@ -0,0 +1,22 @@
+from sqlalchemy import (
+    Table, MetaData, Column,
+    Integer, String, Date,
+    ForeignKey
+)
+from sqlalchemy.orm import mapper, relationship
+import model
+
+metadata = MetaData()
+
+order_lines = Table(
+    'order_lines',
+    metadata,
+    Column('id', Integer, primary_key=True, auto_increment=True),
+    Column('sku', String(255)),
+    Column('quantity', Integer, nullable=False),
+    Column('order_id', String(255))
+)
+
+
+def start_mappters():
+    order_lines_mapper = mapper(model.OrderLine, order_lines)
