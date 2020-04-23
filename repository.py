7e3704244a269b5a24aacb0ed54a56986cd1ca34
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-22 20:47:53 -0400

    Add AbstractRepository

---
+++ repository.py
@@ -0,0 +1,13 @@
+from typing import abc
+import model
+
+
+class AbstractRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> model.Batch:
+        raise NotImplementedError
