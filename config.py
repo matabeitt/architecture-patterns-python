6e5d046d691975ba326ff03f4d9df3e7f4874a28
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-24 20:30:05 -0400

    Fix typo in db uri

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ def database_uri():
     port = 54470 if host == 'localhost' else 5447
     password = os.environ.get('DB_PASSWORD', 'Password1')
     user, db_name = 'allocation', 'allocation'
-    return f'postgesql://{user}:{password}@{host}:{port}/{db_name}'
+    return f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
 
 
 def api_url():
