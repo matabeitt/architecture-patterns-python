85e8b8942e29803646d68d4680184d22e55315b3
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-24 20:28:27 -0400

    Test OrderLine insertion.

---
+++ test_orm.py
@@ -0,0 +1,17 @@
+import pytest
+import model
+
+
+def test_orderline_mapper_can_load_lines(session):
+    session.execute(
+        'INSERT INTO order_lines (order_id, sku, quantity) VALUES '
+        '("order1", "RED-CHAIR", 12),'
+        '("order1", "RED-TABLE", 13),'
+        '("order2", "BLUE-LIPSTICK", 14)'
+    )
+    expected = [
+        model.OrderLine("order1", "RED-CHAIR", 12),
+        model.OrderLine("order1", "RED-TABLE", 13),
+        model.OrderLine("order2", "BLUE-LIPSTICK", 14),
+    ]
+    assert session.query(model.OrderLine).all() == expected
