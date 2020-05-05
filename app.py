b0c078e5fe63a8f70f670b003a4e6e00a0d7ee43
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-05-05 17:36:54 -0400

    Create a session and get request json object.

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, jsonify, make_response
-from model import OrderLine
+from flask import Flask, jsonify, make_response, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import orm
+import model
+import config
+import repository
+
+
+orm.start_mappers()
 app = Flask(__name__)
+get_session = sessionmaker(bind=create_engine(config.database_uri))
 
 
 @app.route('/', methods=['POST'])
 @app.route('/allocate', methods=['POST'])
 def allocate_endpoint():
+    session = get_session()
     # TODO: Extract OrderLine data from query params
+    orderline = (
+        request.json['order_id'],
+        request.json['sku'],
+        request.json['quantity'],
+    )
     # TODO: Load All Batches from DB
     # TODO: Call Domain Service
     # TODO: Save Allocation in DB
